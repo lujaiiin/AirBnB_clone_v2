8a917b4d7971c905bb6927e7736a710c9476e2cb
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-17 01:50:36 +0300

    Update

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import Base
 from models.base_model import BaseModel
-from models.base_model import   Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
@@ -9,9 +9,9 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-     __tablename__ = "users"
-    email = Column (String(128), nullble=Fales)
-    password = Column (String(128), nullble=Fales)
+__tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
