cb316e70061243fb583ecd6d830d71dc50569187
lujain <lujainm.com>
2024-01-17 08:44:25 -0800

    airbnb - sql - project

--- models/place.py
+++ models/place.py
@@ -20,7 +20,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
-    city_id = Column(String(60), ForeignKey("cities.id")nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
@@ -35,9 +35,9 @@ class Place(BaseModel, Base):
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
-         def reviews(self):
-             review_list = []
-             for review in list(models.storage.all(Review).values()):
-                 if review.place_id == self.id:
+        def reviews(self):
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
                     review_list.append(review)
-             return review_list
+            return review_list
--- models/review.py
+++ models/review.py
@@ -4,13 +4,13 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
-from sqlalchemy import Stringfrom 
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-     __tablename__ = "reviews"
+    __tablename__ = "reviews"
     place_id = Column(String(1024), nullable=False)
     user_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     text = Column(String(60), ForeignKey("users.id"), nullable=False)
