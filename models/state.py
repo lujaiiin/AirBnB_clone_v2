12ceaf57d07f3a919c745c8ec543b2a97ef6bf55
lujain <lujainm.com>
2024-01-17 14:51:45 -0800

    airbnb - sql - project

--- models/state.py
+++ models/state.py
@@ -16,14 +16,21 @@ class State(BaseModel):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
         """cites"""
-        list = []
-        ac = models.storage.all(City)
-        for i in ac:
-            if ac[i] == self.id:
-                nlist.append(ac[i])
-        return nlist
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
