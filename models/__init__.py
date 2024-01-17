96edbc146ebf0727672b0dbb3c25d943448fb4c2
lujain <lujainm.com>
2024-01-17 13:45:52 -0800

    airbnb - sql - project

--- models/__init__.py
+++ models/__init__.py
@@ -3,10 +3,16 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
-
 storage.reload()
