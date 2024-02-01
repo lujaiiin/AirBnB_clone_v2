2170b8b47bf5c470c205eb5295d2bd848d440117
lujain <lujainm.com>
2024-02-01 06:08:49 -0800

    airbnb - sql - project

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# archive
+from datetime import datetime
+from fabric.api import local
+import os.path as oo
+
+
+def do_pack():
+    """Create a tar archive"""
+    date = datetime.utcnow()
+    filee = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
+
+    if oo.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+
+    if local("tar -cvzf {} web_static".format(filee)).failed is True:
+        return None
+
+    return filee
