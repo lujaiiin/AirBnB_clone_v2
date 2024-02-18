ca335f0342200da9974df269cd3c0f33c57c5681
lujain <lujainm.com>
2024-02-18 09:38:06 -0800

    airbnb - sql - project

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,38 +2,32 @@
 """Modules"""
 from flask import Flask, request
 
-
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """hello functin"""
-
+    """hello function"""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hhbnb():
-    """hhbnb fun"""
-
+    """hhbnb function"""
     return 'HBNB'
 
-
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/c/', strict_slashes=False)
 def c_text(text):
     """c_text function"""
     text = text.replace('_', ' ')
     return f'C {text}'
 
-
-@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def python_text(text):
     """python_text function"""
     text = text.replace('_', ' ')
     return f'Python {text}'
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
