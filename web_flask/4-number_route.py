ee2126506524f349591f12c4777d4f06c1674887
lujain <lujainm.com>
2024-02-18 09:46:17 -0800

    airbnb - sql - project

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,15 +4,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """hello function"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hhbnb():
     """hhbnb function"""
     return 'HBNB'
+
 
 @app.route('/c/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/c/', strict_slashes=False)
@@ -21,6 +24,7 @@ def c_text(text):
     text = text.replace('_', ' ')
     return f'C {text}'
 
+
 @app.route('/python/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def python_text(text):
@@ -28,11 +32,12 @@ def python_text(text):
     text = text.replace('_', ' ')
     return f'Python {text}'
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """number function"""
     return f'{n} is a number'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
