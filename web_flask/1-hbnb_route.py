a1d1b2e539b4ca8f3b5ef66ea42cd3ebf6b94c92
lujain <lujainm.com>
2024-02-18 08:24:24 -0800

    airbnb - sql - project

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,17 +5,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """hello functin"""
 
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hhbnb():
     """hhbnb fun"""
 
     return 'HBNB'
+
 
 if __name__ == '__main__':
 
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,11 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """hello functin"""
 
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hhbnb():
@@ -17,12 +19,13 @@ def hhbnb():
 
     return 'HBNB'
 
+
 @app.route('/c/<path:text>', strict_slashes=False)
 def c_text(text):
     """c_text function"""
     text = text.replace('_', ' ')
     return f'C {text}'
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
