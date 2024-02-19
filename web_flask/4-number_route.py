815425eb5ada2bd4d9192dea8f23898c70739fec
lujain <lujainm.com>
2024-02-18 16:22:42 -0800

    airbnb - sql - project

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,43 +1,39 @@
 #!/usr/bin/python3
 """Modules"""
-from flask import Flask, request
 
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """hello function"""
+def hello():
+    """fun"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hhbnb():
-    """hhbnb function"""
+def hbnb():
+    """fun"""
     return 'HBNB'
 
 
-@app.route('/c/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/c/', strict_slashes=False)
-def c_text(text):
-    """c_text function"""
-    text = text.replace('_', ' ')
-    return f'C {text}'
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """funC """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python_text(text):
-    """python_text function"""
-    text = text.replace('_', ' ')
-    return f'Python {text}'
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text='is cool'):
+    """funPython"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """number function"""
-    return f'{n} is a number'
-
+def imanum(n):
+    """fun im"""
+    return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
