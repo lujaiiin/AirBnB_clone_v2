4a774580b395e106d28fac3b6dea0297d2985847
lujain <lujainm.com>
2024-02-18 16:06:49 -0800

    airbnb - sql - project

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,63 +1,26 @@
 #!/usr/bin/python3
-from flask import Flask, render_template_string, request
+"""Modules"""
+
+from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-storage.init_app(app)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """code  """
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
+def teardown_db(exception):
+    """fb"""
     storage.close()
 
-
-@app.route('/states', strict_slashes=False)
-def states():
-    states = storage.all(State).order_by(State.name)
-    return render_template_string('''
-    <html>
-        <body>
-            <h1>States</h1>
-            <ul>
-                {% for state in states %}
-                    <li>{{ state.id }}: <b>{{ state.name }}</b></li>
-                {% endfor %}
-            </ul>
-        </body>
-    </html>
-    ''', states=states)
-
-
-@app.route('/states/<int:state_id>', strict_slashes=False)
-def state(state_id):
-    state = storage.get(State, state_id)
-    if state:
-        cities = state.cities.order_by(City.name)
-        return render_template_string('''
-        <html>
-            <body>
-                <h1>State: {{ state.name }}</h1>
-                <h3>Cities:</h3>
-                <ul>
-                    {% for city in cities %}
-                        <li>{{ city.id }}: <b>{{ city.name }}</b></li>
-                    {% endfor %}
-                </ul>
-            </body>
-        </html>
-        ''', state=state, cities=cities)
-    else:
-        return 'Not found!',  404
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
