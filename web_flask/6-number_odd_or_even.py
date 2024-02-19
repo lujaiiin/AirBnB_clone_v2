f58b9abfd8cb45c87f0a514b05428e6d3795d1db
lujain <lujainm.com>
2024-02-18 16:20:08 -0800

    airbnb - sql - project

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,57 +1,56 @@
 #!/usr/bin/python3
 """Modules"""
-from flask import Flask, request, render_template_string
 
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """hello function"""
+def hello():
+    """hello fun"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hhbnb():
-    """hhbnb function"""
+def hbnb():
+    """fun HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<path:text>', defaults={'text': 'is cool'},
-        strict_slashes=False)
-@app.route('/c/', strict_slashes=False)
-def c_text(text):
-    """c_text function"""
-    text = text.replace('_', ' ')
-    return f'C {text}'
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """fun C"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/<path:text>', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python_text(text):
-    """python_text function"""
-    text = text.replace('_', ' ')
-    return f'Python {text}'
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text='is cool'):
+    """py fun """
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """number function"""
-return f'{n} is a number'
+def imanumber(n):
+    """fun"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """number_template function"""
-    return render_template_string('<html><body><h1>Number: {{ n }}</h1></body></html>', n=n)
+def numbersandtemplates(n):
+    """fun"""
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """number_odd_or_even function"""
-    return render_template_string('<html><body><h1>Number: {{ n }} is {{ "even" if n %  2 ==  0 else "odd" }}</h1></body></html>', n=n)
-
+def numbersandevenness(n):
+    """fun"""
+    if n % 2 == 0:
+        evenness = 'even'
+    else:
+        evenness = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           evenness=evenness)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
