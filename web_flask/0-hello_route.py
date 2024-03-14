8cb73c16779f532b460e117c7f1b5d2d65d68c66
lujain <lujain@gmail.com>
2024-03-14 11:27:41 +0000

    airbnb - sql - project

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Modules"""
-from flask import Flask
 
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """hello functin"""
-
     return 'Hello HBNB!'
 
 
